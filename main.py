85c92ea0e435c4b6b9729fb01540321a7ca1fff5
RAHUL DAS <1705255@kiit.ac.in>
2020-11-06 15:32:42 +0000

    Pattern-2

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-n = int(input("Enter the number of rows: "))
-for i in range(1,n+1):
-  for j in range(1,i+1):
-    print(j,end=" ")
-  print()
-
-
-
-for i in range(1,n+1):
-  for j in range(1,i+1):
-    print(i,end=" ")
+n = int(input("Enter the range: "))
+for i in range(n):
+  for j in range(n):
+    if (j==0) or (i==n-1) or (i==j):
+      print("*",end = "")
+    else:
+      print(end = " ")
   print()
